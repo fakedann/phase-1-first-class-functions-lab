062ee82cb6b0bdad6926b5a55c7d1928b3b5d801
Daniel Escalona <daniel07escalona@gmail.com>
2022-01-12 12:45:24 -0500

    commit tree 1952fc2c448af22701bdf7c162b325bc16c1d722

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiply){
+    return function fareMultiplier (fare) {
+        return fare*multiply;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, funciones){
+    return funciones(drivers);
+}
